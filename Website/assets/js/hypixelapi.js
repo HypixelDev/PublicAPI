ffa9f5b71de03acb2211d2696cc29ed1fca65627
ThorinDev <c.nethercott@icloud.com>
2016-07-02 19:32:50 +0100

    Formatting

--- Website/assets/js/hypixelapi.js
+++ Website/assets/js/hypixelapi.js
@@ -1,31 +1,31 @@
 $(document).ready(function(){
-			$('#github').click(function(){
-				reDirect('github');
-			});
-			$('#hypixel').click(function(){
-				reDirect('hypixel');
-			});
-			$('#javadocs').click(function(){
-				reDirect('javadocs');
-			});
-			$('#apihelp').click(function(){
-				reDirect('apihelp');
-			});
-		});
+	$('#github').click(function(){
+		reDirect('github');
+	});
+	$('#hypixel').click(function(){
+		reDirect('hypixel');
+	});
+	$('#javadocs').click(function(){
+		reDirect('javadocs');
+	});
+	$('#apihelp').click(function(){
+		reDirect('apihelp');
+	});
+});
 		
-		function reDirect(Site) {
-			switch(Site) {
-				case 'github':
-					window.location.href='https://github.com/HypixelDev/PublicAPI';
-					break;
-				case 'hypixel':
-					window.location.href='https://hypixel.net';
-					break;
-				case 'javadocs':
-					window.location.href='https://api.hypixel.net/javadocs';
-					break;
-				case 'apihelp':
-					window.location.href='https://hypixel.net/forums/api-help.111/';
-					break;
-			}
-		}
+function reDirect(Site) {
+	switch(Site) {
+		case 'github':
+			window.location.href='https://github.com/HypixelDev/PublicAPI';
+			break;
+		case 'hypixel':
+			window.location.href='https://hypixel.net';
+			break;
+		case 'javadocs':
+			window.location.href='https://api.hypixel.net/javadocs';
+			break;
+		case 'apihelp':
+			window.location.href='https://hypixel.net/forums/api-help.111/';
+			break;
+	}
+}
