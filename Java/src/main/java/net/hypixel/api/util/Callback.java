83427b5b901d97fc75d4796fecbbe9ebc15212ae
mmxw11 <matias.korpela@hotmail.fi>
2016-12-05 12:52:45 +0200

    Mark Callback as a Functional Interface

--- Java/src/main/java/net/hypixel/api/util/Callback.java
+++ Java/src/main/java/net/hypixel/api/util/Callback.java
@@ -2,6 +2,7 @@ package net.hypixel.api.util;
 
 import net.hypixel.api.reply.AbstractReply;
 
+@FunctionalInterface
 public interface Callback<T extends AbstractReply> {
 
     public void callback(Throwable failCause, T result);
