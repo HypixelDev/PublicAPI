6e4cd071588a7e3bb27833bf1b434bdfe96d2788
mmxw11 <matias.korpela@hotmail.fi>
2016-12-05 12:21:39 +0200

    Make callback interface

--- Java/src/main/java/net/hypixel/api/util/Callback.java
+++ Java/src/main/java/net/hypixel/api/util/Callback.java
@@ -2,16 +2,7 @@ package net.hypixel.api.util;
 
 import net.hypixel.api.reply.AbstractReply;
 
-public abstract class Callback<T extends AbstractReply> {
-    private final Class<T> clazz;
+public interface Callback<T extends AbstractReply> {
 
-    public Callback(Class<T> clazz) {
-        this.clazz = clazz;
-    }
-
-    public abstract void callback(Throwable failCause, T result);
-
-    public final Class<T> getClazz() {
-        return clazz;
-    }
+    public void callback(Throwable failCause, T result);
 }
