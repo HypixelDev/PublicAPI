e5b85cfe6a020342a1ca8274afb86b53310c4449
Connor Linfoot <connorlinfoot@me.com>
2021-05-09 22:53:27 +0100

    Update Java/src/main/java/net/hypixel/api/reply/skyblock/SkyBlockProfilesReply.java

--- Java/src/main/java/net/hypixel/api/reply/skyblock/SkyBlockProfilesReply.java
+++ Java/src/main/java/net/hypixel/api/reply/skyblock/SkyBlockProfilesReply.java
@@ -7,7 +7,7 @@ import net.hypixel.api.reply.AbstractReply;
 public class SkyBlockProfilesReply extends AbstractReply {
     private JsonElement profiles;
 
-    public JsonArray getProfile() {
+    public JsonArray getProfiles() {
         if (profiles == null || profiles.isJsonNull()) {
             return null;
         } else {
