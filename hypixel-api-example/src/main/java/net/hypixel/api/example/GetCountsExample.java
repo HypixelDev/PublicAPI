3c1ef7a089dd378282b87b17b8309c0f125f0008
Connor Linfoot <connorlinfoot@me.com>
2021-05-10 12:48:36 +0100

    Be consistent

--- hypixel-api-example/src/main/java/net/hypixel/api/example/GetCountsExample.java
+++ hypixel-api-example/src/main/java/net/hypixel/api/example/GetCountsExample.java
@@ -1,6 +1,6 @@
 package net.hypixel.api.example;
 
-public class CountsExample {
+public class GetCountsExample {
     public static void main(String[] args) {
         ExampleUtil.API.getCounts().whenComplete(ExampleUtil.getTestConsumer());
         ExampleUtil.await();
--- hypixel-api-example/src/main/java/net/hypixel/api/example/GetPunishmentStatsExample.java
+++ hypixel-api-example/src/main/java/net/hypixel/api/example/GetPunishmentStatsExample.java
@@ -1,6 +1,6 @@
 package net.hypixel.api.example;
 
-public class PunishmentStatsExample {
+public class GetPunishmentStatsExample {
     public static void main(String[] args) {
         ExampleUtil.API.getPunishmentStats().whenComplete(ExampleUtil.getTestConsumer());
         ExampleUtil.await();
