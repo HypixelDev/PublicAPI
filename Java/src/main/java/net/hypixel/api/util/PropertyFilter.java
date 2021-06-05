b6a231d48965559b9665cd4891146799ab9d4446
Nullicorn <bennullicorn@gmail.com>
2021-06-05 08:11:03 -0400

    Document PropertyFilter

--- Java/src/main/java/net/hypixel/api/util/PropertyFilter.java
+++ Java/src/main/java/net/hypixel/api/util/PropertyFilter.java
@@ -9,42 +9,111 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 /**
- * @author Nullicorn
+ * A tool for trimming unneeded properties from {@link ComplexHypixelObject}s, especially to
+ * minimize their memory and storage consumption. Based on MongoDB projections.
+ * <p><br>
+ * To use an <strong>inclusion filter</strong>, property names (or "keys") can be added via {@link
+ * #include(String...) include(...)} or the {@link #with(String...) with(...) constructor}. When an
+ * object is {@link #applyTo(ComplexHypixelObject) passed through} the filter, any properties not
+ * explicitly named using the aforementioned methods will be removed from the object. If the object
+ * did not have an included property to begin with, it will not be created.
+ * <p><br>
+ * Property names are referenced using dot-notation. See the documentation for {@link
+ * #include(String...) include(...)} for more details.
  */
 public class PropertyFilter {
 
+    /**
+     * Shorthand for constructing a new filter that only allows the {@code includedKeys} to {@link
+     * #applyTo(ComplexHypixelObject) pass through}. See {@link #include(String...)} for the key
+     * syntax.
+     */
     public static PropertyFilter with(String... includedKeys) {
         PropertyFilter filter = new PropertyFilter();
         filter.include(includedKeys);
         return filter;
     }
 
+    /**
+     * Helper function that validates a {@code keys} array and converts it to a list.
+     */
+    private static List<String> keysToList(String... keys) {
+        if (keys == null || keys.length == 0) {
+            throw new IllegalArgumentException("At least 1 key name required");
+        }
+
+        List<String> keyList = Arrays.asList(keys);
+        for (int i = 0; i < keyList.size(); i++) {
+            String key = keyList.get(i);
+            if (key == null) {
+                throw new IllegalArgumentException("Filtered keys cannot be null");
+            }
+
+            // Remove escape characters.
+            keyList.set(i, key.replace("\\.", "."));
+        }
+
+        return keyList;
+    }
+
+    // Only these keys are allowed in objects returned from `applyTo(...)`.
+    // Keys with literal dots should be stripped of escape characters before being added.
     private final Set<String> allowedKeys;
 
     public PropertyFilter() {
         allowedKeys = new HashSet<>();
     }
 
+    /**
+     * Allows properties with any of the provided {@code keys} to {@link
+     * #applyTo(ComplexHypixelObject) pass through} the filter. To include nested properties, use
+     * dots ({@code .}) to separate each parent property from its child. If a property's name
+     * contains a dot literally, use a double-backslash to escape the dot. (e.g. {@code
+     * "key_with_literal_dot\\.in_it"} instead of {@code "key_with_literal_dot.in_it"})
+     * <br><pre>
+     * Examples:
+     *     •{@code uuid}                - Keep the player's UUID when filtering.
+     *     •{@code stats}               - Keep all of the player's stats when filtering.
+     *     •{@code stats.SkyWars}       - Keep all of the player's SkyWars stats when filtering.
+     *     •{@code stats.SkyWars.coins} - Keep just the player's SkyWars coins when filtering.
+     * </pre>
+     * Any duplicate keys (or keys already included in the filter) will be ignored.
+     *
+     * @param keys Names of properties that will be allowed to pass through the filter (in
+     *             dot-notation).
+     */
     public void include(String... keys) {
         allowedKeys.addAll(keysToList(keys));
     }
 
+    /**
+     * Removes all of the provided keys from the filter, such that objects {@link
+     * #applyTo(ComplexHypixelObject) passed through} the filter will <em>not</em> include
+     * properties with those keys.
+     * <p><br>
+     * Attempting to remove a key that was already removed, or never {@link #include(String...)
+     * included} to begin with, will have no effect.
+     */
     public void removeKeys(String... keys) {
         allowedKeys.removeAll(keysToList(keys));
     }
 
-    private List<String> keysToList(String... keys) {
-        if (keys == null || keys.length == 0) {
-            throw new IllegalArgumentException("At least 1 key name required");
-        }
-
-        List<String> keyList = Arrays.asList(keys);
-        if (keyList.contains(null)) {
-            throw new IllegalArgumentException("Filtered keys cannot be null");
-        }
-        return keyList;
+    /**
+     * @return A new set containing all property keys that can {@link #applyTo(ComplexHypixelObject)
+     * pass through} the filter.
+     * @see #include(String...)
+     */
+    public Set<String> getIncludedKeys() {
+        return new HashSet<>(allowedKeys);
     }
 
+    /**
+     * Strips the {@code object} of any properties that haven't explicitly been allowed via {@link
+     * #include(String...)} or the {@link PropertyFilter#with(String...) with(...) constructor}.
+     * <p><br>
+     * The resulting object will (at most) only contain the properties returned by {@link
+     * #getIncludedKeys()}. Any properties missing from the object will not be added.
+     */
     public void applyTo(ComplexHypixelObject object) {
         JsonObject json = object.getRaw();
         if (json == null || json.entrySet().isEmpty()) {
@@ -59,8 +128,8 @@ public class PropertyFilter {
                 continue;
             }
 
-            // Tokenize the path at un-escaped periods.
-            // The negative-lookbehind ensures that periods are not preceded by a backslash.
+            // Tokenize the path at un-escaped dots.
+            // The negative-lookbehind ensures that dots are not preceded by a backslash.
             String[] tokens = key.split("(?<!\\\\)\\.");
 
             JsonObject parent = temp;
