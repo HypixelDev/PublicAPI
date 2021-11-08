86d3b8e3047101b6124498997c8d0f710c6b0f7e
yawn <55554025+yawn999@users.noreply.github.com>
2021-11-07 21:30:01 -0500

    Update Utilities.java
    code cleanup

--- hypixel-api-core/src/main/java/net/hypixel/api/util/Utilities.java
+++ hypixel-api-core/src/main/java/net/hypixel/api/util/Utilities.java
@@ -16,41 +16,41 @@ import java.util.regex.Pattern;
 
 public final class Utilities {
 
-    private static final Pattern TOKEN_SPLITTER = Pattern.compile("(?<!\\\\)\\.");
-    public static final Gson GSON = new GsonBuilder()
-            .registerTypeAdapter(UUID.class, new UUIDTypeAdapter())
-            .registerTypeAdapter(GameType.class, new GameTypeTypeAdapter())
-            .registerTypeAdapter(ServerType.class, new ServerTypeTypeAdapter())
-            .registerTypeAdapter(ZonedDateTime.class, new DateTimeTypeAdapter())
-            .registerTypeAdapter(Player.class, new PlayerTypeAdapter())
-            .registerTypeAdapterFactory(new BoostersTypeAdapterFactory<>(BoostersReply.Booster.class))
-            .create();
+  private static final Pattern TOKEN_SPLITTER = Pattern.compile("(?<!\\\\)\\.");
+  public static final Gson GSON = new GsonBuilder()
+    .registerTypeAdapter(UUID.class, new UUIDTypeAdapter())
+    .registerTypeAdapter(GameType.class, new GameTypeTypeAdapter())
+    .registerTypeAdapter(ServerType.class, new ServerTypeTypeAdapter())
+    .registerTypeAdapter(ZonedDateTime.class, new DateTimeTypeAdapter())
+    .registerTypeAdapter(Player.class, new PlayerTypeAdapter())
+    .registerTypeAdapterFactory(new BoostersTypeAdapterFactory < > (BoostersReply.Booster.class))
+    .create();
 
-    public static ZonedDateTime getDateTime(long timeStamp) {
-        return Instant.ofEpochMilli(timeStamp).atZone(ZoneId.of("America/New_York"));
+  public static ZonedDateTime getDateTime(long timeStamp) {
+    return Instant.ofEpochMilli(timeStamp).atZone(ZoneId.of("America/New_York"));
+  }
+
+  /**
+   * Splits the input {@code key} into tokens, which are delimited by dots ({@code .}) that aren't
+   * preceded by a backslash ({@code \}).
+   */
+  public static String[] tokenizeKey(String key) {
+    return TOKEN_SPLITTER.split(key);
+  }
+
+  public static UUID uuidFromString(String uuidStr) {
+    if (!uuidStr.contains("-")) {
+      uuidStr = uuidStr.substring(0, 8) + "-" +
+        uuidStr.substring(8, 12) + "-" +
+        uuidStr.substring(12, 16) + "-" +
+        uuidStr.substring(16, 20) + "-" +
+        uuidStr.substring(20, 32);
+
     }
+    return UUID.fromString(uuidStr);
+  }
 
-    /**
-     * Splits the input {@code key} into tokens, which are delimited by dots ({@code .}) that aren't
-     * preceded by a backslash ({@code \}).
-     */
-    public static String[] tokenizeKey(String key) {
-        return TOKEN_SPLITTER.split(key);
-    }
-
-    public static UUID uuidFromString(String uuidStr) {
-        if (!uuidStr.contains("-")) {
-            uuidStr = uuidStr.substring(0, 8) + "-"
-                    + uuidStr.substring(8, 12) + "-"
-                    + uuidStr.substring(12, 16) + "-"
-                    + uuidStr.substring(16, 20) + "-"
-                    + uuidStr.substring(20, 32);
-
-        }
-        return UUID.fromString(uuidStr);
-    }
-
-    private Utilities() {
-        throw new UnsupportedOperationException("Helper class should not be instantiated");
-    }
+  private Utilities() {
+    throw new UnsupportedOperationException("Helper class should not be instantiated");
+  }
 }
