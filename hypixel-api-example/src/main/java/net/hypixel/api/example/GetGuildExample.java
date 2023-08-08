cefffd261315818fc17bea858fd718d0bc681c95
firetrqck <140627932+firetrqck@users.noreply.github.com>
2023-08-08 08:13:53 -0400

    small accuracy fix for guild members
    125 is a in game limit on how many people can join a guild in hypixel, the endpoint can theoretically return infinite.

--- hypixel-api-example/src/main/java/net/hypixel/api/example/GetGuildExample.java
+++ hypixel-api-example/src/main/java/net/hypixel/api/example/GetGuildExample.java
@@ -127,8 +127,9 @@ public class GetGuildExample {
         /*
          * Finally, we'll print some information about each member in the guild.
          *
-         * This might print out upto 125 seperate usernames, so you may want to comment the following line out if you're
-         * focusing on other info.
+         * This might print out a lot usernames(in theory infinity, 
+         * practically upto 125, the *current* in game member limit),
+         * so you may want to comment the following line out if you're focusing on other info.
          */
         printGuildMembers(guild.getMembers());
     }
