9fee6dccb1484e17a35defcf66d52199c89376ec
JustDrven <petr.nemec11111@gmail.com>
2025-02-28 19:25:00 +0100

    feat(config): created config manager for easier configuration

---
+++ hypixel-api-example/src/main/java/net/hypixel/api/example/config/ConfigManager.java
@@ -0,0 +1,35 @@
+package net.hypixel.api.example.config;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigManager {
+
+    private static ConfigManager instance;
+
+    public static ConfigManager getInstance() {
+        if (instance == null) {
+            instance = new ConfigManager();
+        }
+
+        return instance;
+    }
+
+    private final Properties config = new Properties();
+
+    private ConfigManager() {
+        try {
+            String fileName = "config.properties";
+
+            InputStream input = ClassLoader.getSystemResourceAsStream(fileName);
+            config.load(input);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Properties getConfig() {
+        return config;
+    }
+}
